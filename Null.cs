edfe1053b5bcdba2923fa5901dc0091c1bcad4c3
olimov Amir <olimov.amir@mail.ru>
2023-11-30 09:52:00 +0300

    Добавьте файлы проекта.

---
+++ Null.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorNull
+{
+     class Null
+    {
+        static void Main(string[] args)
+        {
+            //string str = "test";
+            string str = null;
+
+          Console.WriteLine(str ?? "not Data");
+
+            
+        }
+    }
+}
